87992b1c05eadcdb83f513fd69d00030b8fd10e6
DESKTOP-DD2BR4M\Front-End Developer <frontendercode@gmail.com>
2019-06-30 10:20:37 +0300

    v.0.11
    Сверстал блок детали

--- app/libs/common.js
+++ app/libs/common.js
@@ -15,7 +15,24 @@ $(document).ready(function () {
 		$('.hidden-mnu').hide("slow");
 	});
 
+const tabLinks = document.querySelectorAll(".tabs a");
+const tabPanels = document.querySelectorAll(".tabs-panel");
 
+for (let el of tabLinks) {
+  el.addEventListener("click", e => {
+    e.preventDefault();
+
+    document.querySelector(".tabs li.active").classList.remove("active");
+    document.querySelector(".tabs-panel.active").classList.remove("active");
+
+    const parentListItem = el.parentElement;
+    parentListItem.classList.add("active");
+    const index = [...parentListItem.parentElement.children].indexOf(parentListItem);
+
+    const panel = [...tabPanels].filter(el => el.getAttribute("data-index") == index);
+    panel[0].classList.add("active");
+  });
+}
 
 
 	//Ajax push mesege http://api.jquery.com/jquery.ajax/
